989b2fbbac5ac4bc390d07417d5ccd34d9a97ee2
Bishal Ghosh <2692726G@student.gla.ac.uk>
2023-04-16 20:05:24 +0100

    final app

--- Dispenser_app/Form1.cs
+++ Dispenser_app/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +10,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 //using System.Windows.Input;
 
 namespace Dispenser_app
@@ -16,26 +18,48 @@ namespace Dispenser_app
     
     public partial class Form1 : Form
     {
-        public static double counter = 0;
+        private NetworkStream stream;
+        private static double counter = 0;
+        private static double balance = 0;
+        bool enable = false;
         //static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
-        public static TcpClient client = new TcpClient();
+        private static TcpClient client = new TcpClient();
         
         public Form1()
         {
+            
             InitializeComponent();
-        }
+            Thread t = new Thread(new ThreadStart(StartReceiving));
+            t.Start();
 
+        }
+        private void StartReceiving()
+        {
+            Int32 port = 13160;
+            IPAddress ipAddr = IPAddress.Parse("172.20.10.2");
+            var ipEndPoint = new IPEndPoint(ipAddr, port);
+            TcpClient client = new TcpClient();
+            client.Connect(ipEndPoint);
+            while (true)
+            {
+                NetworkStream stream = client.GetStream();
+                if (client.ReceiveBufferSize > 0)
+                {
+                    var buffer = new byte[client.ReceiveBufferSize];
+                    int received = stream.Read(buffer, 0, client.ReceiveBufferSize);
+                    var message = Encoding.UTF8.GetString(buffer);
+                    balance = double.Parse("52.8725945", System.Globalization.CultureInfo.InvariantCulture);
+                    enable = true;
+                }
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            IPHostEntry ipHostInfo = System.Net.Dns.GetHostEntry("serveraddress.com");
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            client.Connect(ipAddress, 21);
-            client.SendTimeout = 3000;
-            var status = client.Connected;
-            Console.WriteLine(status);
+            
         }
         private async void Form1_KeyPress(object sender, KeyPressEventArgs e)
-        {
+        {   
+            if (!enable) return;
             //Console.WriteLine("Here");
             if (char.IsWhiteSpace(e.KeyChar))
             {
@@ -46,10 +70,22 @@ namespace Dispenser_app
             }
 
         }
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        private async void Form1_KeyUp(object sender, KeyEventArgs e)
         {
+            if (!enable) return;
             string fuel = label1.Text;
             string cost = label2.Text;
+            var message = fuel+","+cost;
+            Int32 port = 8009;
+            IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+            var ipEndPoint = new IPEndPoint(ipAddr, port);
+            TcpListener listener = new TcpListener(ipEndPoint);
+            listener.Start();
+            TcpClient handler = await listener.AcceptTcpClientAsync();
+            NetworkStream stream = handler.GetStream();
+            var encodedmsg = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(encodedmsg, 0, encodedmsg.Length);
+            enable = false;
             counter = 0;
         }
         private void discharge_Click(object sender, EventArgs e)
--- Dispenser_app/Program.cs
+++ Dispenser_app/Program.cs
@@ -16,7 +16,8 @@ namespace Dispenser_app
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Form1 Dispform = new Form1();
+            Application.Run(Dispform);
         }
     }
 }
